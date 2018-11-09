2ed6d6f85cd8fa6db03ce2eb3cb2591d7af43fc8
seul lee <blacksangi@gmail.com>
2018-11-09 19:04:32 +0900

    뒤로가기 버튼

--- MyWebBrowser/app/src/main/java/io/github/lee_seul/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/io/github/lee_seul/mywebbrowser/MainActivity.kt
@@ -29,4 +29,12 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
