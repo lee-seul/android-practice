b6d91155e3e485fd089565d83f6f905b12a442fb
seul lee <blacksangi@gmail.com>
2018-11-12 22:16:27 +0900

    to-do 리스트

---
+++ TodoList/app/src/main/java/com/lee/seul/todolist/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.lee.seul.todolist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import io.realm.Realm
+import io.realm.Sort
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jetbrains.anko.startActivity
+
+class MainActivity : AppCompatActivity() {
+
+    val realm = Realm.getDefaultInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val realmResult = realm.where<Todo>()
+            .findAll()
+            .sort("date", Sort.DESCENDING)
+
+        val adapter = TodoListAdapter(realmResult)
+        list_v.adapter = adapter
+
+        realmResult.addChangeListener { _ -> adapter.notifyDataSetChanged() }
+
+        list_v.setOnItemClickListener { parent, view, position, id ->
+            startActivity<EditActivity>("id" to id)
+        }
+
+        fab.setOnClickListener { view ->
+            startActivity<EditActivity>()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        realm.close()
+    }
+}
